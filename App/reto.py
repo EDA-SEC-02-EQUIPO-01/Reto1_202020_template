6b98f173fcaaddddd06c0599447d995d06f08386
dcamelog <69636867+dcamelog@users.noreply.github.com>
2020-09-01 23:02:04 -0500

    Update reto.py

--- App/reto.py
+++ App/reto.py
@@ -33,6 +33,8 @@ import csv
 from ADT import list as lt
 from DataStructures import listiterator as it
 from DataStructures import liststructure as lt
+from Sorting import mergesort as mr
+from Sorting import shellsort as sh
 
 from time import process_time 
 
@@ -64,7 +66,7 @@ def compareRecordIds (recordA, recordB):
 
 
 def loadCSVFile (file, cmpfunction):
-    lst=lt.newList("ARRAY_LIST", cmpfunction)
+    lst=lt.newList("SINGLE_LINKED", cmpfunction)
     dialect = csv.excel()
     dialect.delimiter=";"
     try:
@@ -78,9 +80,143 @@ def loadCSVFile (file, cmpfunction):
 
 
 def loadMovies ():
-    lst = loadCSVFile("theMoviesdb/movies-small.csv",compareRecordIds) 
+    lst = loadCSVFile("theMoviesdb/SmallMoviesDetailsCleaned.csv",compareRecordIds) 
     print("Datos cargados, " + str(lt.size(lst)) + " elementos cargados")
     return lst
+def loadCasting ():
+    lst = loadCSVFile("theMoviesdb/MoviesCastingRaw-small.csv",compareRecordIds) 
+    print("Datos cargados, " + str(lt.size(lst)) + " elementos cargados")
+    return lst
+
+def less(item1,item2,etiqueta="vote_average"):
+    if float(item1[etiqueta])<float(item2[etiqueta]):
+        return True
+    return False
+def greater(item1,item2,etiqueta="vote_average"):
+    if float(item1[etiqueta])>float(item2[etiqueta]):
+        return True
+    return False
+def less_count(item1,item2,etiqueta="vote_count"):
+    if int(item1[etiqueta])<int(item2[etiqueta]):
+        return True
+    return False
+def greater_count(item1,item2,etiqueta="vote_count"):
+    if int(item1[etiqueta])>int(item2[etiqueta]):
+        return True
+    return False
+def promediar(lst,caracteristica):
+    total=0
+    for i in range(lt.size(lst)):
+        total+=float(lst['elements'][i][caracteristica])
+    return round((total/lt.size(lst)),1)
+
+def CrearRankingPelicula(calificacion, lst, orden,rango=10,seguro=False):
+    """
+    Retorna una lista con cierta cantidad de elementos ordenados por el criterio
+    """
+    if lst['size']==0:
+        print("La lista esta vacía")  
+        return 0
+    else:
+        t1_start = process_time() #tiempo inicial
+        counter={}
+        almacen={}
+        if calificacion.lower()=="peor":
+            if orden==0:
+                mr.mergesort(lst,less_count)
+                for i in range(1,rango+1):
+                    counter=lt.getElement(lst,i)
+                    if not seguro:    
+                        print(f"{counter['title']}:{counter['vote_count']}")
+                    almacen[counter['title']]=counter['vote_count']
+            else:    
+                mr.mergesort(lst,less)
+                for f in range(1,rango+1):
+                    counter=lt.getElement(lst,f)
+                    if not seguro:
+                        print(f"{counter['title']}:{counter['vote_average']}")
+                    almacen[counter['title']]=counter['vote_count']
+        elif calificacion.lower()=="mejor":
+            if orden==0:
+                mr.mergesort(lst,greater_count)
+                for i in range(1,rango+1):
+                    counter=lt.getElement(lst,i)
+                    if not seguro:
+                        print(f"{counter['title']}:{counter['vote_count']}")
+                    almacen[counter['title']]=counter['vote_count']
+            else:    
+                mr.mergesort(lst,greater)
+                for i in range(1,rango+1):
+                    counter=lt.getElement(lst,i)
+                    if not seguro:
+                        print(f"{counter['title']}:{counter['vote_average']}")
+                    almacen[counter['title']]=counter['vote_count']
+        t1_stop=process_time()
+        
+        print(f"tiempo de ejecucion: {t1_stop-t1_start} segundos")
+        return almacen
+
+def conversor_entre_cvs(id_movie,iterador):
+    while it.hasNext(iterador):
+        counter=it.next(iterador)
+        if counter['id']==id_movie:
+            return counter
+
+def conocer_un_director(lst,lst2, director,etiqueta="director_name",criterio_calificacion="vote_average"):
+    if lst['size']==0:
+        print("La lista esta vacía")  
+        return 0
+    else:
+        t1_start = process_time() #tiempo inicial
+        coleccion=lt.newList("ARRAY_LIST")
+        iterador_coleccion=it.newIterator(lst)
+        while it.hasNext(iterador_coleccion):
+            counter=it.next(iterador_coleccion)
+            if director.lower() in counter[etiqueta].lower():
+                id_store=counter['id']
+                iterador_pelicula=it.newIterator(lst2)
+                pelicula_convertida=conversor_entre_cvs(id_store,iterador_pelicula)
+                lt.addLast(coleccion,pelicula_convertida)
+                print(lt.lastElement(coleccion)['title'])
+        promedio=promediar(coleccion,criterio_calificacion)
+        print(f"Peliculas totales:{lt.size(coleccion)}\nCalificacion promedio:{promedio}")
+        
+def conocer_un_genero(lst,director,seguro=False):
+    if lst['size']==0:
+        print("La lista esta vacía")  
+        return 0
+    else:
+        t1_start = process_time() #tiempo inicial
+        coleccion=lt.newList("ARRAY_LIST")
+        iterador_coleccion=it.newIterator(lst)
+        while it.hasNext(iterador_coleccion):
+            counter=it.next(iterador_coleccion)
+            if director.lower() in counter["genres"].lower():
+                lt.addLast(coleccion,counter)
+                if not seguro:
+                    print(lt.lastElement(coleccion)['title'])
+        promedio=promediar(coleccion,"vote_count")
+        print(f"Peliculas totales:{lt.size(coleccion)}\nCalificacion promedio:{promedio}")
+    return coleccion
+
+def ranking_de_genero(lst,genero,criterio,ordenamiento,num):
+    if lst['size']==0:
+        print("La lista esta vacía")  
+        return 0
+    else:
+        t1_start = process_time() #tiempo inicial
+        lista_genero=conocer_un_genero(lst,genero,True)
+        lista_ordenada_genero=CrearRankingPelicula(criterio,lista_genero,ordenamiento,num,True)
+        if criterio=="mejor":
+            if ordenamiento==0:
+                print(f"{num} BEST COUNTS MOVIES!!\n{lista_ordenada_genero}")
+            elif ordenamiento==1:
+                print(f"{num} BEST AVERAGE MOVIES!!\n{lista_ordenada_genero}")
+        elif criterio=="peor":
+            if ordenamiento==0:
+                print(f"{num} WORST COUNTS MOVIES!!\n{lista_ordenada_genero}")
+            elif ordenamiento==1:
+                print(f"{num} WORST AVERAGE MOVIES!!\n{lista_ordenada_genero}")
 
 
 def main():
@@ -91,8 +227,6 @@ def main():
     Args: None
     Return: None 
     """
-
-
     while True:
         printMenu() #imprimir el menu de opciones en consola
         inputs =input('Seleccione una opción para continuar\n') #leer opción ingresada
@@ -100,22 +234,29 @@ def main():
 
             if int(inputs[0])==1: #opcion 1
                 lstmovies = loadMovies()
+                lstcasting=loadCasting()
 
             elif int(inputs[0])==2: #opcion 2
-                pass
+                orden=int(input("Ingrese 0 para ordenar segun numero de votos\nIngrese 1 para ordenar segun calificacion promedio\n"))
+                criterio=input("Ingrese 'mejor'(sin comillas) para ranking de mejores peliculas\nIngrese 'peor'(sin comillas) para ranking de peores peliculas\n")
+                CrearRankingPelicula(criterio,lstmovies,orden)
 
             elif int(inputs[0])==3: #opcion 3
-                pass
+                director=input("Ingrese el nombre del director a buscar\n")
+                conocer_un_director(lstcasting,lstmovies,director)
 
             elif int(inputs[0])==4: #opcion 4
                 pass
 
-            elif int(inputs[0])==3: #opcion 5
-                pass
-
-            elif int(inputs[0])==4: #opcion 6
-                pass
-
+            elif int(inputs[0])==5: #opcion 5
+                genero=input("Ingrese el genero que desea entender\n")
+                conocer_un_genero(lstmovies,genero)
+            elif int(inputs[0])==6: #opcion 6
+                genero=input("Ingrese el genero que desea un ranking\n")
+                orden=int(input("Ingrese 0 para ordenar segun numero de votos\nIngrese 1 para ordenar segun calificacion promedio\n"))
+                criterio=input("Ingrese 'mejor'(sin comillas) para ranking de mejores peliculas\nIngrese 'peor'(sin comillas) para ranking de peores peliculas\n")
+                num=int(input("Ingrese el numero de peliculas que desea ver en el ranking\n"))
+                ranking_de_genero(lstmovies,genero,criterio,orden,num)
 
             elif int(inputs[0])==0: #opcion 0, salir
                 sys.exit(0)
